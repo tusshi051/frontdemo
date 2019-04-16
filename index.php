76403efb51041b8d369a0b5da6d5a1847b9bd276
Tusshi051 <tusharvaghasiya82@gmail.com>
2019-04-16 12:46:56 +0530

    commit

---
+++ index.php
@@ -0,0 +1,499 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Landing PAGE">
+    <title>Demo</title>
+
+    <link href="assets/js/plugins/bootsnav_files/skins/color.css" rel="stylesheet">
+
+    <link href="assets/js/plugins/bootsnav_files/css/animate.css" rel="stylesheet">
+
+    <link href="assets/js/plugins/bootsnav_files/css/bootsnav.css" rel="stylesheet">
+
+    <link href="assets/js/plugins/bootsnav_files/css/overwrite.css" rel="stylesheet">
+
+    <link href="assets/js/plugins/owl-carousel/owl.carousel.css" rel="stylesheet">
+
+    <link href="assets/js/plugins/owl-carousel/owl.theme.css" rel="stylesheet">
+
+    <link href="assets/js/plugins/owl-carousel/owl.transitions.css" rel="stylesheet">
+
+    <link href="assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/magnific-popup.css" rel="stylesheet">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link href="assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <link href="assets/css/main.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+    <div class="page-preloader">
+
+        <div class="spinner">
+
+            <div class="rect1"></div>
+
+            <div class="rect2"></div>
+
+            <div class="rect3"></div>
+
+            <div class="rect4"></div>
+
+            <div class="rect5"></div>
+
+        </div>
+
+    </div>
+
+    <!--//** Navigation**//-->
+
+    <nav class="navbar navbar-default navbar-fixed white no-background bootsnav navbar-scrollspy" data-minus-value-desktop="70" data-minus-value-mobile="55" data-speed="1000">
+
+        <div class="container">
+
+            <!-- Start Header Navigation -->
+
+            <div class="navbar-header">
+
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-menu">
+
+                    <i class="fa fa-bars"></i>
+
+                </button>
+
+                <a class="navbar-brand" href="#">
+                    
+                    <h2>Logo</h2>
+
+                </a>
+
+            </div>
+
+            <!-- End Header Navigation -->
+
+            <div class="collapse navbar-collapse" id="navbar-menu">
+
+                <ul class="nav navbar-nav navbar-right">
+
+                    <li class="active scroll"><a href="#home">Home</a></li>
+
+                    <li class="scroll"><a href="#about">Smooth Scroll</a></li>
+                    
+                    <li class="scroll"><a href="#features">Gradient</a></li>
+                    
+                    <li class="scroll"><a href="#clippig">Clippig</a></li>
+                    
+                    <li class="scroll"><a href="#popup">Popup</a></li>
+                    
+                    <li class="scroll"><a href="#data_fetch">Fetch Data</a></li>
+
+                    <li class="button-holder">
+
+                        <button type="button" class="btn btn-orng navbar-btn" data-toggle="modal" id="modal_btn" data-target="#SignIn">Sign in</button>
+
+                    </li>
+
+                </ul>
+
+            </div>
+
+        </div>
+
+    </nav>
+
+    <!--//** Banner**//-->
+
+    <section id="home" >
+
+        <div class="container">
+
+            <div class="row mhead" >
+
+                <div class="col-md-6 caption">
+
+                    <h1 class="wel">Welcome To <span class="wel1">Lorem Ipsum</span></h1>
+
+                    <h2>  Lorem Ipsum
+
+                            <span class="animated-text"></span>
+
+                            <span class="typed-cursor"></span>
+
+                    </h2>
+                    <p class="tag_header">Lorem Ipsum is simply dummy text of the printing and typesetting industry. </p>
+
+                </div>
+                </div>
+                <div class="header__graphic-box">
+                    
+                </div>
+        </div>
+
+    </section>
+
+    <section id="about" class="section-padding" style="padding-bottom: 150px;">
+
+        <div class="container">
+
+            <h2 class="scrl">Smooth Scroll</h2>
+
+            <p class="sub_scrl"><span>&#8220;</span>Lorem Ipsum is not simply random text.<span>&#8221;</span></p>
+
+            <div class="row">
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+
+                    <div class="icon-box" style=" background-color: #B576A5;">
+
+                        <img style="width: 64px;" src="assets/img/player.png" class="material-icons">
+
+                        <h4>Watch Videos</h4>
+
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+
+                    <div class="icon-box" style=" background: #86CBEB;">
+
+                        <img style="width: 64px;" src="assets/img/aurvey badge.png" class="material-icons">
+
+                        <h4>Surveys</h4>
+
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+
+                    <div class="icon-box" style=" background: #CBAACA;">
+
+                        <img style="width: 70px;" src="assets/img/task icon.png" class="material-icons">
+
+                        <h4>Tasks</h4>
+
+                    </div>
+
+                </div>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+
+                    <div class="icon-box" style=" background: #CE7A94;">
+
+                        <img style="width: 70px;" src="assets/img/games v.png" class="material-icons">
+
+                        <h4>Games &amp; Apps</h4>
+
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+
+                    <div class="icon-box" style="background: #A989CC;">
+
+                        <img style="width: 70px;" src="assets/img/search.png" class="material-icons">
+
+                        <h4>clicks &amp; websurfing</h4>
+
+                    </div>
+
+                </div>
+
+                <div class="col-md-4 col-sm-6 col-xs-12">
+
+                    <div class="icon-box" style="background: #FFEDE9;">
+
+                        <img style="width: 70px;" src="assets/img/free-trials.png" class="material-icons">
+
+                        <h4>Free Trials</h4>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </section>
+
+    <section id="features">
+    
+        <div class="container">
+           
+
+            <div class="row">
+
+                <div class="col-md-10">
+
+                    <h2>Gradient</h2>
+
+                    <hr>
+
+                    <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+
+                </div>
+
+            </div>
+        </div>
+
+    </section>
+    
+    <section id="contact" class="section-padding bg-img" >
+
+        <div class="contact-forms">
+            <div class="container">
+                <div class="col-md-3 col-sm-4 col-xs-12 gradient_l" >
+                    <h3 class="col-md-3 col-sm-4 col-xs-12 gradient_w">Gradient with Layer</h3>
+                </div>
+                
+            </div>
+            
+        </div>
+    </section>
+    <section id="clippig" class="section-padding ">
+
+        <div class="contact-forms">
+            <h2 class="clip_g">Clipping</h2>
+            <div class="container">
+                <div class="clip-wrap">
+                    <img src="https://static.pexels.com/photos/192/wood-landscape-nature-rocks-medium.jpg" width="180" height="180" class="clip-polygon">
+                    <img src="https://static.pexels.com/photos/397/italian-landscape-mountains-nature-medium.jpg" width="180" height="180" class="clip-polygon">
+                    <img src="https://static.pexels.com/photos/1562/italian-landscape-mountains-nature-medium.jpg" width="180" height="180" class="clip-polygon">
+                    <img src="https://static.pexels.com/photos/1242/nature-field-sun-agriculture-medium.jpg" width="180" height="180" class="clip-polygon">
+                    <img src="https://static.pexels.com/photos/541/nature-forest-trees-fog-medium.jpg" width="180" height="180" class="clip-polygon">
+                    <img src="https://static.pexels.com/photos/1130/snow-landscape-mountains-nature-medium.jpg" width="180" height="180" class="clip-polygon">
+                    <img src="https://static.pexels.com/photos/792/wood-landscape-mountains-nature-medium.jpg" width="180" height="180" class="clip-polygon">
+                </div>
+            </div>
+            
+        </div>
+    </section>
+    
+    <section id="popup" class="section-padding">
+
+        <div class="container b_pad_50" >
+
+            <h2 class="pop_p">Popup</h2>
+
+        </div>
+        <div class="contact-forms">
+
+            <div class="container text-center">
+                <button type="button" class="btn btn-orng navbar-btn" data-toggle="modal" id="modal_btn" data-target="#SignIn">Click here</button>
+            </div>
+
+        </div>
+
+    </section>
+    <section id="data_fetch" class="section-padding" style="padding-bottom: 150px;">
+
+        <div class="container">
+
+            <h2 class="scrl" style="margin-bottom: 50px; text-align: center;">Fetch data from Instagram</h2>
+
+            <div class="row">
+                <?php 
+                    $url = 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fweb.stagram.com%2Frss%2Fn%2Fbmw';
+                                         //open connection
+                    $ch = curl_init();
+                    curl_setopt($ch,CURLOPT_URL, $url);
+                    curl_setopt($ch, CURLOPT_FORBID_REUSE, 1);
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+                    $re = curl_exec($ch);
+                    curl_close($ch);
+
+                    $redy = json_decode($re);
+                    if($redy->status != "error"){
+                        $photos = array();
+                        for ($x = 0; $x <= 5; $x++) {
+                            $mystring = $redy->items[$x]->description;
+                            $parts = explode("<",$mystring); 
+                            $mystring = $parts['0'];
+                            $photos[] = array('thumbnail'=> $redy->items[$x]->thumbnail);
+                        }
+                    }
+
+                    ?>
+                <?php foreach ($photos as $photos){ ?>
+                <div class="col-md-4 col-sm-6 col-xs-12">
+
+                        <img style="width: 100%;margin-top: 20px;" src="<?php echo $photos['thumbnail']; ?>" class="material-icons">
+
+                </div>
+                <?php } ?>
+
+            </div>
+
+        </div>
+
+    </section>
+    
+    <!-- Modal -->
+
+    <div class="modal fade" id="SignIn" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+        <div class="modal-dialog" role="document">
+
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    
+                    <h4 class="modal-title text-center" id="myModalLabel">Sign In</h4>
+
+                </div>
+
+                <div class="modal-body">
+
+                    <form class="signup-form" action="" method="post" role="form" >
+
+                        <div class="form-group">
+                            <input type="email" class="form-control my_form"  name="email" placeholder="Email Address" value="" required="required"/>
+                        </div>
+
+                        <div class="form-group">
+                            <input type="password" class="form-control my_form" name="password" id="password" placeholder="Password" required="required"/>
+                        </div>
+
+                        <div class="form-group text-center" style="width: 175px;">
+                            <input type="submit" class="btn btn-blue btn-block" name="submit" value="Log In"/>
+                        </div>
+
+                    </form>
+
+                </div>
+
+                <div class="modal-footer text-center">
+
+                    <a href="#"  data-toggle="modal" id="forgot_btn" data-target="#forgot">Forgot your password </a>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+    
+    <div class="modal fade" id="forgot" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+        <div class="modal-dialog" role="document">
+
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+                    <h4 class="modal-title text-center" id="myModalLabel">Recover Password</h4>
+
+                </div>
+
+                <div class="modal-body">
+
+                    <form class="signup-form" action="" method="post" role="form" >
+
+                        <div class="form-group">
+                            <input type="email" class="form-control my_form"  name="email" placeholder="Email Address" value="" required="required"/>                            
+                        </div>
+
+                        <div class="form-group text-center" style="width: 175px;">
+                            <input type="submit" class="btn btn-blue btn-block" name="submit" value="Submit"/>
+                        </div>
+
+                    </form>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+    <div class="modal fade" id="contact_pop" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+
+        <div class="modal-dialog" id="cont_modaldia" role="document">
+
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+
+                    <h4 class="modal-title text-center" id="myModalLabel">Contact Us</h4>
+
+                </div>
+
+                <div class="modal-body">
+
+                    <form id="queryform" class="contact-form" method="post" role="form">
+
+                        <div class="col-md-6">
+
+                            <div class="form-group">
+
+                                <input type="text" class="form-control" name="name" placeholder="Full Name" required="required">
+                                
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+
+                                <input type="email" class="form-control"  name="email" placeholder="Email" required="required"/>
+                                
+                            </div>
+
+                        </div>
+
+                        <div class="col-md-12" style=" margin-bottom: 15px;">
+
+                            <div class="form-group">
+
+                                <textarea class="form-control" rows="3" name="message" placeholder="Message"></textarea>
+                                
+                            </div>
+
+                        </div>
+                        <input type="submit"  class="btn btn-blue btn-block snd_msg sub_btn" name="submit" value="Send Message"/>
+
+                    </form>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <script src="assets/bootstrap-3.3.7/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/plugins/owl-carousel/owl.carousel.min.js"></script>
+
+    <script src="assets/js/plugins/bootsnav_files/js/bootsnav.js"></script>
+
+    <script src="assets/js/plugins/typed.js-master/typed.js-master/dist/typed.min.js"></script>
+
+    <script src="assets/js/plugins/Magnific-Popup-master/Magnific-Popup-master/dist/jquery.magnific-popup.js"></script>
+
+    <script src="assets/js/main.js"></script>
+    
+</body>
+</html>
